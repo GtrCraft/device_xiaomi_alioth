75c14f79a1957eabc40bd1d829655b28342191fa
GtrCraft <nickvanbokhorst@hotmail.com>
2021-10-26 13:46:26 +0200

    alioth: Unleash the kraken

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/potato_alioth.mk
+    $(LOCAL_DIR)/aosp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    potato_alioth-user \
-    potato_alioth-userdebug \
-    potato_alioth-eng
+    aosp_alioth-user \
+    aosp_alioth-userdebug \
+    aosp_alioth-eng
